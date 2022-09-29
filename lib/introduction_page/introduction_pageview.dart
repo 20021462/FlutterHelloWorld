206a195fa9fc6d1812206748e47241fa0ca1d51d
na1811 <80148215+na1811@users.noreply.github.com>
2022-09-30 02:12:39 +0700

    new introduction page (#2)

---
+++ lib/introduction_page/introduction_pageview.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+import '../home_page.dart';
+
+class IntroductionPageView extends StatefulWidget {
+  const IntroductionPageView({Key key}) : super(key: key);
+
+  @override
+  State<IntroductionPageView> createState() => _IntroductionPageViewState();
+}
+
+class _IntroductionPageViewState extends State<IntroductionPageView> {
+  final controller = PageController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  Widget buildPage({
+    String urlImage,
+    String title,
+    String subtitle,
+  }) =>
+      Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 40,),
+            Image.asset(
+              urlImage,
+              fit: BoxFit.cover,
+              width: 350,
+            ),
+            SizedBox(height: 10,),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Text(
+                subtitle,
+                style: TextStyle(
+                  color: Colors.teal.shade700,
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    final shape = RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(15),
+    );
+
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(bottom: 230),
+        child: PageView(
+          controller: controller,
+          children: [
+            buildPage(
+                urlImage: 'assets/images/illustrations1.png',
+                title: 'Gain total control of your money',
+                subtitle: 'Become your own money manager and make every cent count'
+            ),
+            buildPage(
+                urlImage: 'assets/images/illustrations2.png',
+                title: 'Know where your money goes',
+                subtitle: 'Track your transaction easily,with categories and financial report'
+            ),
+            buildPage(
+                urlImage: 'assets/images/illustrations3.png',
+                title: 'Planning ahead',
+                subtitle: 'Setup your budget for each category so you in control'
+            ),
+
+          ],
+        ),
+      ),
+      bottomSheet: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        height: 230,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Center(
+              child: SmoothPageIndicator(
+                controller: controller,
+                count: 3,
+                effect: ScrollingDotsEffect(
+                  activeDotScale: 2,
+                  spacing: 20,
+                  dotColor: Colors.grey.shade300,
+                  activeDotColor: Color(0xff7f3dff),
+                  dotHeight: 8,
+                  dotWidth: 8,
+                ),
+
+                onDotClicked: (index) =>
+                    controller.animateToPage(
+                        index,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeIn),
+              ),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(380,56),
+                  shape: shape,
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),)
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Color(0xff7f3dff),
+                  backgroundColor: Colors.grey.shade300,
+                  minimumSize: Size(380,56),
+                  shape: shape,
+                ),
+                onPressed: () async {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                  );
+                },
+                child: Text("Login",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),)
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hello_world/introduction_page/introduction_pageview.dart';
 import 'back_end/moma_user.dart';
 
-// const MaterialColor primaryBlack = MaterialColor(
-//   _blackPrimaryValue,
-//   <int, Color>{
-//     50: Color(0xFF000000),
-//     100: Color(0xFF000000),
-//     200: Color(0xFF000000),
-//     300: Color(0xFF000000),
-//     400: Color(0xFF000000),
-//     500: Color(_blackPrimaryValue),
-//     600: Color(0xFF000000),
-//     700: Color(0xFF000000),
-//     800: Color(0xFF000000),
-//     900: Color(0xFF000000),
-//   },
-// );
-// const int _blackPrimaryValue = 0xFF000000;
+const MaterialColor primaryBlack = MaterialColor(
+  _blackPrimaryValue,
+  <int, Color>{
+    50: Color(0xFF000000),
+    100: Color(0xFF000000),
+    200: Color(0xFF000000),
+    300: Color(0xFF000000),
+    400: Color(0xFF000000),
+    500: Color(_blackPrimaryValue),
+    600: Color(0xFF000000),
+    700: Color(0xFF000000),
+    800: Color(0xFF000000),
+    900: Color(0xFF000000),
+  },
+);
+const int _blackPrimaryValue = 0xFF000000;
 
-void main() async {
+void main() async{
   MomaUser appUser = MomaUser("test@gmail.com");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp(
-    appUser: appUser,
-  ));
+  runApp(MyApp(appUser: appUser,));
 }
 
-// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
   MomaUser appUser;
 
-  MyApp({Key key, this.appUser}) : super(key: key);
+  MyApp({Key key,this.appUser}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MOMA',
-      theme: ThemeData(),
-      home: HomePage(appUser: appUser),
-      //home: NewPage(),
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: primaryBlack,
+      ),
+      home: IntroductionPageView(),
+      //home: MainScreen(appUser: appUser,),
       debugShowCheckedModeBanner: false,
       // routes: <String, WidgetBuilder>{
       //   '/signup': (BuildContext context) => SignupPage(),
