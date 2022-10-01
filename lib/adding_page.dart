148583f2cec3a25d0fd956f99940111ec394fb50
Alter <crimson.lotus.201@gmail.com>
2022-10-01 18:44:06 +0700

    Squashed commit of the following:
    commit 6bf743911916c9a7b3b3bf1363dfe6e8d3949c1c
    Author: Alter <crimson.lotus.201@gmail.com>
    Date:   Sat Oct 1 18:39:00 2022 +0700
    
        1/10/22: Update AddingPage UI
    
        - adding_page.dart : almost finish, needs to replace the temporary DropDownButton with the Wallet list and repaint the whole page.
    
    commit ee921323f55af8de259d72f7c0a56dbc3ddab236
    Author: Alter <crimson.lotus.201@gmail.com>
    Date:   Fri Sep 30 12:30:43 2022 +0700
    
        30/9/22: Update the UI for Adding page
    
        - Add the prototype appbar (works for every page)
        - Add the Inter family font(regular and semibold only)
        - 2 new dependencies: intl & flutter_masked_text
        - WIP Adding page, can't input data yet

--- lib/adding_page.dart
+++ lib/adding_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/back_end/group_money.dart';
 import 'package:hello_world/back_end/moma_user.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 // ignore: must_be_immutable
 class AddingPage extends StatefulWidget {
@@ -12,36 +14,206 @@ class AddingPage extends StatefulWidget {
 }
 
 class _AddingPageState extends State<AddingPage> {
-  int _initvalue = 0;
+  String categoryDropDownValue = groupMoneyList[0].name;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      color: const Color(0xFFFFF6E6),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            "Slider",
-            style: TextStyle(fontSize: 30),
+          SafeArea(
+            child: Container(
+              margin: const EdgeInsets.only(left: 30, right: 30, top: 60),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'How much?',
+                    style: TextStyle(
+                      fontSize: 19,
+                      fontWeight: FontWeight.w600,
+                      color: Color.fromARGB(170, 10, 0, 0),
+                    ),
+                  ),
+                  TextField(
+                    controller: MoneyMaskedTextController(
+                      rightSymbol: 'đ',
+                      thousandSeparator: ',',
+                      decimalSeparator: '',
+                      precision: 0,
+                    ),
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Input your money',
+                    ),
+                    keyboardType: const TextInputType.numberWithOptions(
+                      signed: false,
+                      decimal: false,
+                    ),
+                    style: const TextStyle(fontSize: 40),
+                  ),
+                ],
+              ),
+            ),
           ),
-          Text(
-            '$_initvalue',
-            style: const TextStyle(fontSize: 120),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius:
+                    BorderRadiusDirectional.vertical(top: Radius.circular(45)),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.only(
+                top: 30,
+                left: 20,
+                right: 20,
+              ),
+              margin: const EdgeInsets.only(top: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: ListView(
+                      physics: const NeverScrollableScrollPhysics(),
+                      padding: const EdgeInsets.all(0),
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 20),
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black,
+                                  style: BorderStyle.solid,
+                                  width: 1.0,
+                                ),
+                                borderRadius: BorderRadius.circular(25),
+                                color: Colors.white,
+                              ),
+                              child: DropdownButtonFormField(
+                                isDense: false,
+                                alignment: Alignment.centerLeft,
+                                itemHeight: 60,
+                                decoration: const InputDecoration.collapsed(
+                                    hintText: ''),
+                                value: categoryDropDownValue,
+                                onChanged: (category) {
+                                  setState(() {
+                                    categoryDropDownValue = category;
+                                  });
+                                },
+                                items: groupMoneyList.map((group) {
+                                  return DropdownMenuItem(
+                                    alignment: AlignmentDirectional.center,
+                                    value: group.name,
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        group.icon,
+                                        const SizedBox(width: 10),
+                                        Text(group.name),
+                                      ],
+                                    ),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 20),
+                              padding: const EdgeInsets.only(left: 10),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black,
+                                  style: BorderStyle.solid,
+                                  width: 1.0,
+                                ),
+                                borderRadius: BorderRadius.circular(25),
+                                color: Colors.white,
+                              ),
+                              height: 60,
+                              alignment: AlignmentDirectional.centerStart,
+                              child: TextFormField(
+                                decoration: const InputDecoration.collapsed(
+                                  hintText: 'Description',
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(bottom: 20),
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black,
+                                  style: BorderStyle.solid,
+                                  width: 1.0,
+                                ),
+                                borderRadius: BorderRadius.circular(25),
+                                color: Colors.white,
+                              ),
+                              child: DropdownButtonFormField(
+                                isDense: false,
+                                alignment: Alignment.centerLeft,
+                                itemHeight: 60,
+                                decoration: const InputDecoration.collapsed(
+                                    hintText: ''),
+                                value: categoryDropDownValue,
+                                onChanged: (category) {
+                                  setState(() {
+                                    categoryDropDownValue = category;
+                                  });
+                                },
+                                items: groupMoneyList.map((group) {
+                                  return DropdownMenuItem(
+                                    alignment: AlignmentDirectional.center,
+                                    value: group.name,
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        group.icon,
+                                        const SizedBox(width: 10),
+                                        Text(group.name),
+                                      ],
+                                    ),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: double.infinity,
+                    margin: const EdgeInsets.only(bottom: 25),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple.shade400,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Continue',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          Slider(
-            value: _initvalue.toDouble(),
-            min: 0,
-            max: 100,
-            divisions: 10,
-            activeColor: Colors.black,
-            inactiveColor: Colors.grey,
-            //label: 'Set value',
-            onChanged: (double newValue) {
-              setState(() {
-                _initvalue = newValue.round();
-              });
-            },
-          )
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:hello_world/introduction_page/introduction_pageview.dart';
 import 'back_end/moma_user.dart';
 
-const MaterialColor primaryBlack = MaterialColor(
-  _blackPrimaryValue,
-  <int, Color>{
-    50: Color(0xFF000000),
-    100: Color(0xFF000000),
-    200: Color(0xFF000000),
-    300: Color(0xFF000000),
-    400: Color(0xFF000000),
-    500: Color(_blackPrimaryValue),
-    600: Color(0xFF000000),
-    700: Color(0xFF000000),
-    800: Color(0xFF000000),
-    900: Color(0xFF000000),
-  },
-);
-const int _blackPrimaryValue = 0xFF000000;
-const mainColor = Color(0xff7f3dff);
-final shape = RoundedRectangleBorder(
-  borderRadius: BorderRadius.circular(15),
-);
+// const MaterialColor primaryBlack = MaterialColor(
+//   _blackPrimaryValue,
+//   <int, Color>{
+//     50: Color(0xFF000000),
+//     100: Color(0xFF000000),
+//     200: Color(0xFF000000),
+//     300: Color(0xFF000000),
+//     400: Color(0xFF000000),
+//     500: Color(_blackPrimaryValue),
+//     600: Color(0xFF000000),
+//     700: Color(0xFF000000),
+//     800: Color(0xFF000000),
+//     900: Color(0xFF000000),
+//   },
+// );
+// const int _blackPrimaryValue = 0xFF000000;
 
-void main() async{
+void main() async {
   MomaUser appUser = MomaUser("test@gmail.com");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp(appUser: appUser,));
+  runApp(MyApp(
+    appUser: appUser,
+  ));
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
   MomaUser appUser;
 
-  MyApp({Key key,this.appUser}) : super(key: key);
+  MyApp({Key key, this.appUser}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -43,23 +41,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MOMA',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        fontFamily: 'Inter',
       ),
-      home: IntroductionPageView(),
-      //home: MainScreen(appUser: appUser,),
+      home: HomePage(appUser: appUser),
+      //home: NewPage(),
       debugShowCheckedModeBanner: false,
-       routes: <String, WidgetBuilder>{
-         '/home_page': (BuildContext context) => HomePage(appUser: appUser ,),
-      },
+      // routes: <String, WidgetBuilder>{
+      //   '/signup': (BuildContext context) => SignupPage(),
+      //   '/reset_password': (BuildContext context) => ResetPassword(),
+      //   '/main_screen': (BuildContext context) => MainScreen(appUser: appUser ,),
+      // },
     );
   }
 }
